0ba20d6908258854650d2c35c74c682c27696ea4
gituser12981u2 <paullwall@protonmail.com>
2024-06-18 14:31:43 -0700

    Use spawn for starting bot in tests to prevent hanging

--- test/start.test.js
+++ test/start.test.js
@@ -1,15 +1,20 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const assert = require('assert');
 
 describe('Bot Start', function() {
     it('should start without errors',
         function(done) {
             this.timeout(10000); // 10 seconds timeout
-            const bot = exec('node src/index.js', (error, stdout, stderr) => {
-                if (error && error.signal !== 'SIGINT') {
+            const bot = spawn('node', ['src/index.js']);
+
+            let hasExited = false;
+
+            bot.on('error', (error) => {
+                if (!hasExited) {
+                    hasExited = true;
                     done(error);
                 }
-            });
+            });            
 
             setTimeout(() => {
                 bot.kill('SIGINT');
